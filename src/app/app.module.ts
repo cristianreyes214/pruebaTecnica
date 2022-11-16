54a6dcf554aa1c485e6ff8cf8eb1e89c60ed2fb4
cristianreyes214 <cristian.reyes@uao.edu.co>
2022-11-15 20:59:10 -0500

    creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
+// Modulos
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routing
 import { AppRoutingModule } from './app-routing.module';
+
+// Componentes
 import { AppComponent } from './app.component';
+import { PersonajesComponent } from './Componentes/personajes/personajes.component';
+import { InicioComponent } from './Componentes/inicio/inicio.component';
+import { LocacionComponent } from './Componentes/locacion/locacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonajesComponent,
+    InicioComponent,
+    LocacionComponent
   ],
   imports: [
     BrowserModule,
