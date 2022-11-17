16f40268f8ab82898416123d6306952d489eaaa0
cristianreyes214 <cristian.reyes@uao.edu.co>
2022-11-17 18:10:32 -0500

    Enlaces de enrutamiento y funcion

--- src/app/Componentes/inicio/inicio.component.ts
+++ src/app/Componentes/inicio/inicio.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SerieService } from '../../Servicio/serie.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -10,14 +11,25 @@ export class InicioComponent implements OnInit {
 
   personajes: any [] = [];
 
-  constructor(private servicio:SerieService) {
+  constructor(private servicio:SerieService, private router: Router) {
     servicio.obtenerPersonajes().subscribe((res: any) => {
       this.personajes = res.results;
-      console.log(res.results);
     })
   }
 
   ngOnInit(): void {
+  }
+
+  abrir_locacion(locacion: any){
+    if (locacion.id){
+      this.router.navigate(['/locacion', locacion.id]);
+    }
+    else{
+      this.servicio.filtrarLocacion(locacion.name).subscribe((res: any) => {
+        const id = res[0].id;
+        this.router.navigate(['/locacion', id]);
+      });
+    }
   }
 
 }
