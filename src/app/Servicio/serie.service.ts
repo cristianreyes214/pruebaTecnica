b100032aeca1c88fee4d35d0159ad13331ad0191
cristianreyes214 <cristian.reyes@uao.edu.co>
2022-11-17 09:12:45 -0500

    Se crearon dos funciones para traer las locaciones y episodios del personaje

--- src/app/Servicio/serie.service.ts
+++ src/app/Servicio/serie.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -26,6 +27,15 @@ export class SerieService {
 
   multiplesEpisodios(numbers:any[]){
     return this.query(`episode/${numbers}`);
+  }
+
+  filtrarEpisodio( id: string | number, name: string ) {
+    return this.query(`episode/${id}/?name=${name}`);
+  }
+
+  filtrarLocacion( name: string ) {
+    return this.query(`location/?name=${name}`)
+    .pipe(map(data => data['results']));
   }
 }
 
