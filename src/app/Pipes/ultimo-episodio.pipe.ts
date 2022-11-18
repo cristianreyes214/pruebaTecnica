0a0e82b2cb4a8cf084cf65e5802df66da9d68cf7
cristianreyes214 <cristian.reyes@uao.edu.co>
2022-11-18 02:21:10 -0500

    documentacion pipes

--- src/app/Pipes/ultimo-episodio.pipe.ts
+++ src/app/Pipes/ultimo-episodio.pipe.ts
@@ -8,15 +8,23 @@ export class UltimoEpisodioPipe implements PipeTransform {
 
   constructor(private servicio:SerieService){}
 
+  /**
+   * Filtrar informacion de los personajes para encontrar toda la data del ultimo episodio y ultima locacion de cada personaje
+   * @param datos array de lista de cada personaje
+   * @returns array de lista de cada personaje con ultima locacion y ultimo episodio
+   */
   transform( datos: any ): string {
     for ( let i = 0; datos.length > i; i++ ){
+      // Se obtiene ultimo capitulo del personaje
       const posicion = datos[i].episode.length-1;
       const id = datos[i].episode[posicion].split('episode/').pop();
 
+      // Una vez se obtiene la data del episonio, lo reemplazamos para mostrar únicamente el nombre del episodio
       this.servicio.filtrarEpisodio( id, datos[i].name ).subscribe(( res: any ) => {
         datos[i].episode = res.name;
       });
 
+      // Busqueda de la ultima locacion de cada personaje
       this.servicio.filtrarLocacion( datos[i].name.split(' ').shift() ).subscribe(( res:any ) => {
         datos[i].location = res[res.length - 1];
       }, (err) => {
